e8357e2274bb08380e013146dae618c25c8672bd
Damon Verreault <39389306+Izunah@users.noreply.github.com>
2023-03-10 19:01:10 -0700

    Complete exercise 04-removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removeThis ) {
+    let newArray = [];
+    let addMe = true;
 
+    for(let i = 0; i < array.length; i++)
+    {
+        addMe = true;
+        for(let j = 0; j < removeThis.length; j++)
+        {
+            if(array[i] === removeThis[j])
+            {
+                addMe = false;
+            }
+        }
+
+        if(addMe)
+        {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
