0680c0fb39e0babf1e5d921d64e2212a5dda932b
Damon Verreault <39389306+Izunah@users.noreply.github.com>
2023-03-10 18:35:22 -0700

    Complete exercise 03-reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversed = "";
+    let char = "";
+    let pos = 0;
+    
+    for(let i = 1; i <= string.length; i++)
+    {
+        pos = string.length - i;
+        char = string.charAt(pos);
+        reversed += char;
+    }
 
+    return reversed;
 };
 
 // Do not edit below this line
