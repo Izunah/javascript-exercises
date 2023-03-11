c1137d6ea9143f4875a4a10deff79878dcb389f3
Damon Verreault <39389306+Izunah@users.noreply.github.com>
2023-03-10 19:30:59 -0700

    Complete exercise 05-sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let smallerNum = num1;
+    let biggerNum = num2;
 
+
+    if(num1 < 0 || num2 < 0 || typeof(num1) != "number" || typeof(num2) != "number")
+    {
+        return "ERROR";
+    }
+
+    if(num1 >= num2)
+    {
+        smallerNum = num2;
+        biggerNum = num1;
+    }
+
+    for(let i = smallerNum; i <= biggerNum; i++)
+    {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
