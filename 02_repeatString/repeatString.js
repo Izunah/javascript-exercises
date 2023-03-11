851c14f5c2a2e511a8ae1f3c093c861308f5bc1d
Damon Verreault <39389306+Izunah@users.noreply.github.com>
2023-03-10 17:09:53 -0700

    Complete exercise 02-repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeatAmount) {
+    if(repeatAmount >= 0)
+    {
+        return string.repeat(repeatAmount);
+    }
+    else
+    {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
