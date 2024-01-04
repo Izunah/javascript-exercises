cce0ab521ae41599fa869437ed69fe2659db8855
Damon Verreault <39389306+Izunah@users.noreply.github.com>
2024-01-03 20:38:01 -0700

    Complete exercise 08-calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(obj) {
+  let sum = 0;
+  for(val in obj)
+  {
+    if(Number.isInteger(obj[val]))
+      sum += obj[val];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(obj) {
+  let sum = 1;
+  for(val in obj)
+  {
+    if(Number.isInteger(obj[val]))
+      sum *= obj[val];
+  }
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(val) {
+  let factor = 1;
+  for(let i = 0; i < val; i++)
+  {
+    factor = (val - i) * factor;
+  }
+  return factor;
 };
 
 // Do not edit below this line
