bd78eca397913e50b1fccabbb90da6b70fa0a032
Damon Verreault <39389306+Izunah@users.noreply.github.com>
2024-01-03 21:08:20 -0700

    Complete exercise 09-palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let removePunctuation = string.replace(/[^\w\']/g, "").toLowerCase();
+    let reverseString = removePunctuation.split("").reverse().join("");
+    return reverseString == removePunctuation;
 };
 
 // Do not edit below this line
